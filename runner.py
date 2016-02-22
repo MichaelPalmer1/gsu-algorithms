300bb1f266b45046a57b2fa4d7452f35e5a13bdf
Michael Palmer <michael@michaeldpalmer.com>
2016-02-22 14:54:59 -0500

    Implementing the runner

---
+++ runner.py
@@ -0,0 +1,25 @@
+from glob import glob
+from sys import stderr
+
+programs = {}
+n = 1
+
+# Print and save list of programs
+print('Programs:')
+for f in glob('*.py'):
+    if f != 'runner.py':
+        programs[n] = f
+        print('%d. %s' % (n, f))
+        n += 1
+
+# Read which program to run
+program = int(raw_input('Enter program number: '))
+print('')
+
+# Verify a valid program was selected
+if program >= n:
+    stderr.write('Invalid selection')
+    exit()
+
+# Run the program
+execfile(programs[program])
