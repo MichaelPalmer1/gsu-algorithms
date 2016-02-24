6868638530e67530dac5ae0284330a8b9284e55a
Michael Palmer <michael@michaeldpalmer.com>
2016-02-24 15:05:40 -0500

    Update levenshtein distance

--- levenshtein.py
+++ levenshtein.py
@@ -2,6 +2,12 @@ from time import time
 
 
 def levenshtein_distance(s, t):
+    """
+    Calculate the Levenshtein Distance between two strings
+    :param s: String 1
+    :param t: String 2
+    :return: Distance
+    """
     # Base cases
     if s == t:
         return 0
@@ -10,7 +16,7 @@ def levenshtein_distance(s, t):
     if len(t) == 0:
         return len(s)
 
-    # Work vectors
+    # Create work vectors
     v0, v1 = [i for i in range(len(t) + 1)], [0 for _ in range(len(t) + 1)]
 
     for i in range(len(s)):
@@ -19,18 +25,21 @@ def levenshtein_distance(s, t):
         # edit distance is delete (i+1) chars from s to match empty t
         v1[0] = i + 1
 
-        # use formula to fill in the rest of the row
         for j in range(len(t)):
             cost = 0
+
+            # Characters are different
             if s[i] != t[j]:
                 cost = 1
+
+            # Find the minimum
             v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
 
-        # copy v1 (current row) to v0 (previous row) for next iteration
+        # Copy the current row to the previous row (for the next iteration)
         for j in range(len(v0)):
             v0[j] = v1[j]
 
-    return v1[len(t)]
+    return v1[-1]
 
 while True:
     # Get input
