fa7af3ecacb2abe8c43d1daf27af30064359d7af
Michael Palmer <michael@michaeldpalmer.com>
2016-03-23 07:55:10 -0400

    Adding some comments and moving things around

--- README.md
+++ README.md
@@ -12,4 +12,13 @@ contiguous subarray. Output the starting and ending indices and the sum of this 
 Calculate the Levenshtein Distance
 
 ## Master Method
+The formula for the master theorem is as follows:
+
+```
 T(n) = aT(n/b) + Θ(n^k * log^i (n))
+```
+
+The purpose of the master theorem is to calculate the complexity of a divide and conquer expression that is divided 
+into `a` sub-problems, with each sub-problem being of size `n/b`. `n` represents the size of the entire problem and 
+`f(n)` represents the cost outside of the recursive call, such as the cost of merging the sub-problems back together. 
+The end goal is to determine an asymptotically tight bound for the problem.
--- master_method.py
+++ master_method.py
@@ -1,28 +1,59 @@
 # coding=utf-8
+# Specifying the encoding so that we can use some symbols (i.e. theta, superscripts, etc.)
 """
 Michael Palmer (900757121)
 CSCI 5330 A
 March 23, 2016
 
 Master Theorem
--------------------------
 
-T(n) = aT(n/b) + Θ(n^k * log^i (n))
+The formula for the master theorem is as follows: T(n) = aT(n/b) + Θ(n^k * log^i (n)). The purpose of the master
+theorem is to calculate the complexity of a divide and conquer expression that is divided into a sub-problems,
+with each sub-problem being of size n/b. n represents the size of the entire problem and f(n) represents the cost
+outside of the recursive call, such as the cost of merging the sub-problems back together. The end goal is to determine
+an asymptotically tight bound for the problem.
 
 """
 import re
 from math import log
 
+# Console output formatting
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+END = '\033[0m'
+
+# Define the test cases
+cases = [
+    # Case 1
+    [
+        '2T(n/2) + 15',             # Example 1
+        '8T(n/2) + n^2 log^2 n'     # Example 2
+    ],
+
+    # Case 2
+    [
+        '9T(n/9) + n log n',        # Example 1
+        '2T(n/4) + √n'              # Example 2
+    ],
+
+    # Case 3
+    [
+        'T(n/2) + 5n',              # Example 1
+        '4T(n/2) + n^3'             # Example 2
+    ]
+]
+
 
 def master_method(a, b, k, i):
     """
-    Perform master method calculations
-    Formula: T(n) = aT(n/b) + Θ(n^k * log^i (n))
+    Perform master method calculation and print the results
 
-    :param a: Number of problems
-    :param b: Size of the sub-problems
-    :param k: Exponent of n
-    :param i: Exponent of the log
+    :param a: Number of problems (a > 0)
+    :param b: Size of the sub-problems (b > 1)
+    :param k: Exponent of n (k >= 0)
+    :param i: Exponent of the log (i >= 0)
     """
     # Validate input
     if a <= 0:
@@ -34,37 +65,39 @@ def master_method(a, b, k, i):
     if i < 0:
         raise Exception('i must be >= 0')
 
-    # Print the problem
+    # Format the problem and print it out
     print('T(n) = %sT(n/%d) + ϴ(%s)' % ('' if a == 1 else a, b, format_output(k, i)))
 
     # Print problem parts
     print('a = %s, b = %s, k = %s, i = %s' % (a, b, k, i))
+    fn = format_output(k, i)
 
-    # Calculate log base b of a
+    # Calculate log base b of a and print the calculation
     c = log(a, b)
-    print('log%s%d = %.2f' % (format_numbers(b, False), a, c))
+    tn = format_output(c, 0)
+    # Using subscript beta here since subscript b does not seem to exist anywhere and beta looks similar to a b
+    print('c = logᵦa = log%s%d = %.2f' % (format_numbers(b, False), a, c))
+    print('T(n) ∈ ϴ(%s), F(n) ∈ ϴ(%s)' % (tn, fn))
 
-    # Determine the case
+    # Determine the case, format the output, and print the comparison
     if c > k:       # Case 1
-        case = 1
         output = format_output(c, 0)
+        print('Case 1 because T(n) > F(n) (since c > k)')
     elif c == k:    # Case 2
-        case = 2
-        output = format_output(k, i + 1)
+        output = format_output(c, i + 1)
+        print('Case 2 because T(n) ~= F(n) (since c == k)')
     else:           # Case 3
-        case = 3
         output = 'F(n)'
+        print('Case 3 because T(n) < F(n) (since c < k)')
 
-    # Identify the case
-    print('This is case %d' % case)
-
-    # Print the solution
-    print('T(n) = ϴ(%s)\n' % output)
+    # Print the solution (formatted so it stands out)
+    print((BOLD + GREEN + 'T(n) = ϴ(%s)\n' + END) % output)
 
 
 def format_numbers(x, super_script=True):
     """
-    Format a set of numbers as a superscript or subscript
+    Format a series of numbers as a superscript or subscript
+
     :param x: Numbers to format
     :param super_script: Superscript (True) or subscript (False)
     :return: Numbers as superscript/subscript
@@ -88,14 +121,15 @@ def format_numbers(x, super_script=True):
         for s in x_str:
             output += numbers[int(s)]
     else:
-        # Decimals don't play nice, so using the default format if one exists
+        # Decimals don't look good, so falling back to the default format
         output += '^%0.2f' % x
     return output
 
 
 def format_output(k, i):
     """
-    Format the output
+    Format the expression output based on the values of k and i
+
     :param k: Exponent of n
     :param i: Exponent of the log
     :return: String of the formatted output
@@ -110,13 +144,13 @@ def format_output(k, i):
     else:               # Exponent
         output = 'n%s' % format_numbers(k)
 
-    if i != 0:          # Logarithm
+    if i > 0:          # Logarithm
         output += ' log' if output != '' else 'log'
 
-        # Add the exponent
-        output += format_numbers(i) if i != 1 else ''
+        # Append the exponent if needed
+        output += format_numbers(i) if i > 1 else ''
 
-        # Add the n
+        # Finally, append the n
         output += ' n'
 
     return output
@@ -124,33 +158,41 @@ def format_output(k, i):
 
 def process_input(string):
     """
-    Process input string to a, b, k, i variables
+    Process input expression string to a, b, k, i variables
+
     :param string: Expression to evaluate
     :return: (a, b, k, i)
     :rtype: tuple
     """
     a, b, k, i = 0, 0, 0, 0
-    # Grab the relevant parts of the expression
+
+    # Compile the regular expression
     p = re.compile(
         '(?P<a>\d*)T\(n/(?P<b>\d+)\)(?: \+ (?:(?P<n>√n|sqrt\(n\)|\d*n?\^?(?P<k>\d*)) ?(?P<log>log\^?(?P<i>\d*))? ?n?))?'
         , re.IGNORECASE
     )
+
+    # Check for matches
     matches = p.match(string)
     if not matches:
         raise Exception('No matching expression')
 
+    # Convert groups to a dict
     groups = matches.groupdict()
+
+    # If a is not provided, fall back to 1
     a = int(groups['a']) if groups['a'] else 1
     b = int(groups['b'])
 
-    # Check if there is a F(n)
+    # Check if there is a F(n) in the expression
     if groups['n'] or groups['log']:
         # Check if n^k was specified
         nk = groups['n']
         if nk:
+            # Convert to lowercase, just to be safe
             nk = nk.lower()
             if '√' in nk or 'sqrt' in nk:
-                # Square root
+                # Found a square root
                 k = 0.5
             elif '^' in nk:
                 # n^k has an exponent
@@ -169,45 +211,32 @@ def process_input(string):
                 # Just log n
                 i = 1
 
+    # Return the values
     return a, b, k, i
 
 
 def main():
     """
-    Run the program
+    Run the program using the pre-defined test cases
     """
     # Print header
     print('CSCI 5330 Spring 2016')
     print('Michael Palmer')
     print('900757121\n')
 
-    # Define the test cases
-    cases = [
-        # Case 1
-        [
-            '2T(n/2) + 15',
-            '8T(n/2) + n^2 log^2 n'
-        ],
-
-        # Case 2
-        [
-            '9T(n/9) + n log n',
-            '2T(n/4) + √n'
-        ],
-
-        # Case 3
-        [
-            'T(n/2) + 5n',
-            '4T(n/2) + n^3'
-        ]
-    ]
-
     # Loop through the test cases
     for case_num, case in enumerate(cases):
         for example, item in enumerate(case):
-            print("Case %d Example %d" % (case_num + 1, example + 1))
+            # Print case and example numbers (formatted so it stands out)
+            print((UNDERLINE + BLUE + BOLD + "Case %d Example %d" + END) % (case_num + 1, example + 1))
+
+            # Print the expression exactly as it was entered
             print('T(n) = %s' % item)
+
+            # Process the expression
             a, b, k, i = process_input(item)
+
+            # Perform the master method
             master_method(a, b, k, i)
 
 
